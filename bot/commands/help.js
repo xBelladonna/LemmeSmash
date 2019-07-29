5af1ad7be36e37419b8c8547361d3137e3639f4f
xBelladonna <isabelladonnamoore@outlook.com>
2019-07-29 14:13:27 +0930

    Add help command

---
+++ bot/commands/help.js
@@ -0,0 +1,78 @@
+const { Permissions } = require("discord.js");
+const utils = require("../utils.js");
+const config = require("../../config.json");
+
+
+module.exports = {
+    name: "help",
+    aliases: ["h", "info", "invite"],
+    description: "Shows my help card to help you get started",
+    execute: async (client, msg, args) => {
+        if (!args.length) { // If the command is issued on its own
+            const oauthApp = await client.fetchApplication(); // Fetch the bot info to get the client id
+            const flags = config.permissions.proxy.concat(config.permissions.commands); // Compile permission flags into a single array
+            const permissions = Permissions.resolve(flags); // then resolve them to an integer
+            // Construct the URL with proper client ID and permissions integer
+
+            const inviteUrl = `https://discordapp.com/oauth2/authorize?client_id=${oauthApp.id}&scope=bot&permissions=${permissions}`; // Construct invite URL with proper parameters
+            const githubUrl = "https://github.com/xBelladonna/LemmeSmash"; // GitHub repo
+
+            // Basic info about the bot
+            const infoMsg = `LemmeSmash is a Discord bot that lets you put placeholder tags in your messages and replaces your message, swapping the tags out for randomly generated keysmashes. Why would anyone want this? Beats me, but you'll be surprised how fun it can be.\nThe bot uses configurable keysmash tags and generates ISO Standard keysmashes using the characters \`${config.defaultCharset}\` by default, or from a set of custom characters you choose.`;
+
+            // Quick how-to guide
+            gettingStarted = `
+            **1.** Set a keysmash tag: \`ks;tags $smash\`
+            **2.** \`[Optional]\` Set a custom character set: \`ks; charset asdfcvbn\`
+            **3.** Post a message with your keysmash tags: \`Here is a keysmash: $\`
+            Using a set of keysmash tags that you set, it will replace all instances of the tags with a randomly generated keysmash.
+            **4.** \`[Bonus!]\` You can also post a set of characters between your keysmash tags and the bot will use those to generate the keysmash instead: \`Here is a one - time custom keysmash: $sdjcbn\``;
+
+            // Extra tips and tricks
+            footnotes = "• **[Coming soon!]** You can also find out who sent a proxied message by reacting to it with ❓\n• **[Coming soon!]** You can delete proxied messages you sent by reacting to them with ❌";
+
+            // Construct the embed to send
+            embed = utils.successEmbed()
+                .setColor("#ffaa00")
+                .addField("About Me!", infoMsg)
+                .addField("Getting Started:", gettingStarted)
+                .addField("Other things", footnotes)
+                .addField("Add me!", `[Click here to invite the the bot to your server!](${inviteUrl})`)
+                .addField("See my code!", `[Click here to visit the GitHub repository!](${githubUrl})`)
+                .setFooter(`Type \`${config.prefix}help [command name]\` to get info on a specific command!`);
+
+            // Finally, yeet it into the channel
+            return msg.channel.send(embed);
+        }
+
+        // Do the rest of the stuff if there are any arguments
+        const data = [];
+        const commands = msg.client.commands;
+        const name = args[0].toLowerCase();
+        const command = commands.get(name) || commands.find(command => command.aliases && command.aliases.includes(name));
+
+        if (!command) return msg.channel.send(utils.errorEmbed(`Unknown command \`${name}\`. For a list of commands, type \`${config.prefix} help\`, or just ping me!`));
+
+        data.push(`**Name:** ${command.name}`);
+
+        embed = utils.warnEmbed()
+            .setFooter(`Type \`${config.prefix}help [command name]\` to get info on a specific command!`);
+        embed.addField("Command:", command.name);
+        if (command.aliases) embed.addField(`Aliases:`, `${command.aliases.join(', ')}`);
+        if (command.description) embed.addField(`Description:`, `${command.description}`);
+        if (command.usage) {
+            usage = "";
+            for (var i = 0; i < command.usage.length; i++) {
+                if (i < command.usage.length && usage.length + `\n**${config.prefix}${command.name}  ${command.usage[i]}`.length > 1024) {
+                    embed.addField("Usage:", usage);
+                    usage = "";
+                }
+                usage += `\n**${config.prefix}${command.name}  ${command.usage[i]}`;
+            }
+            embed.addField("Usage:", usage);
+        }
+        if (command.example) embed.addField(`Example:`, `${config.prefix}${command.name} ${command.example}`);
+
+        return msg.channel.send(embed);
+    }
+}
