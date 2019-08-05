cc53de95c33be6b4affd5e7608231de95316d8b9
xBelladonna <isabelladonnamoore@outlook.com>
2019-08-05 17:41:50 +0930

    Implement duplicate proxy tag check

--- bot/commands/tags.js
+++ bot/commands/tags.js
@@ -20,11 +20,14 @@ module.exports = {
             if (doc == null) {
                 let newUser = await new user({
                     _id: msg.author.id,
-                    tags: {
+                    keysmash: {
                         prefix: "",
                         suffix: ""
                     },
-                    charset: ""
+                    owo: {
+                        prefix: "",
+                        suffix: "",
+                    }
                 });
                 doc = newUser;
             }
@@ -46,32 +49,39 @@ async function setTags(user, msg, type, args) {
     let response;
     const tagsConflict = utils.errorEmbed(`You've already used either that prefix or suffix for your ${type} tags! Please try again with different tags or type \`${config.prefix}show\` to see your current settings.`);
 
-    if (type == "owospeak") args.shift();
+    if (type === "owospeak") args.shift();
     if (args.length > 0) {
         let proxy = args.join(" ");
-        if (proxy.includes("text") == false) return msg.channel.send(utils.errorEmbed("Example match must contain the string \`text\`, i.e. \`$text$\`"));
+        if (!proxy.includes("text")) return msg.channel.send(utils.errorEmbed("Example match must contain the string \`text\`, i.e. \`$text$\`"));
 
         proxy = proxy.split("text");
         let prefix = proxy[0].trim() || "";
         let suffix = proxy[1].trim() || "";
-        if (prefix == "" && suffix == "") return msg.channel.send(utils.errorEmbed(`Cannot have empty ${type} tags! You must provide either a prefix, a suffix, or both, i.e. \`$text\``));
-        /* TODO: Do this properly at some point to check for dupes
-        if (type == "keysmash") {
-            if (user.owo && (prefix === (user.owo.prefix || user.owo.suffix) || suffix === (user.owo.prefix || user.owo.suffix)))
-                return msg.channel.send(tagsConflict);
-        }
-        else if (type == "owospeak") {
-            if (user.keysmash && (prefix === (user.keysmash.prefix || user.keysmash.suffix) || suffix === (user.keysmash.prefix || user.keysmash.suffix)))
-                return msg.channel.send(tagsConflict);
-        }
-        */
+        if (prefix === "" && suffix === "") return msg.channel.send(utils.errorEmbed(`Cannot have empty ${type} tags! You must provide either a prefix, a suffix, or both, i.e. \`$text\``));
 
-            tags = {
+        // TODO: Refactor this entire block, it's fugly
+        if (type === "keysmash") {
+            if (prefix !== "" && suffix === "" && prefix === (user.owo.prefix || user.owo.suffix))
+                return msg.channel.send(tagsConflict);
+            else if (prefix === "" && suffix !== "" && suffix === (user.owo.prefix || user.owo.suffix))
+                return msg.channel.send(tagsConflict);
+            else if (prefix !== "" && suffix !== "" && prefix === (user.owo.prefix || user.owo.suffix) && suffix === (user.owo.prefix || user.owo.suffix))
+                return msg.channel.send(tagsConflict);
+        } else if (type === "owospeak") {
+            if (prefix !== "" && suffix === "" && prefix === (user.keysmash.prefix || user.keysmash.suffix))
+                return msg.channel.send(tagsConflict);
+            else if (prefix === "" && suffix !== "" && suffix === (user.keysmash.prefix || user.keysmash.suffix))
+                return msg.channel.send(tagsConflict);
+            else if (prefix !== "" && suffix !== "" && prefix === (user.keysmash.prefix || user.keysmash.suffix) && suffix === (user.keysmash.prefix || user.keysmash.suffix))
+                return msg.channel.send(tagsConflict);
+        }
+
+        tags = {
             prefix: prefix,
             suffix: suffix
         }
-        if (tags.prefix != "" && tags.suffix == "") response = `Set ${type} prefix to \`${prefix}\``;
-        else if (tags.prefix == "" && tags.suffix != "") response = `Set ${type} suffix to \`${suffix}\``;
+        if (tags.prefix !== "" && tags.suffix === "") response = `Set ${type} prefix to \`${prefix}\``;
+        else if (tags.prefix === "" && tags.suffix !== "") response = `Set ${type} suffix to \`${suffix}\``;
         else response = `Set ${type} prefix to \`${prefix}\` and suffix to \`${suffix}\``;
     } else {
         tags = {
@@ -81,8 +91,8 @@ async function setTags(user, msg, type, args) {
         response = `Cleared ${type} tags`;
     }
 
-    if (type == "keysmash") user.keysmash = tags;
-    else if (type == "owospeak") user.owo = tags;
+    if (type === "keysmash") user.keysmash = tags;
+    else if (type === "owospeak") user.owo = tags;
 
     return await user.save(err => {
         if (err) throw err;
