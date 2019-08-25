1d06a875c952691bb1628f687d59e491709320a8
xBelladonna <isabelladonnamoore@outlook.com>
2019-08-25 23:11:27 +0930

    Add official server link to invite card

--- bot/commands/invite.js
+++ bot/commands/invite.js
@@ -8,8 +8,15 @@ module.exports = {
     usage: [""],
     examples: null,
     execute: async (client, msg, args) => {
+        const author = await client.user;
+        const guildMember = msg.channel.type === "text" ? await msg.guild.fetchMember(author) : false;
         const flags = config.permissions.proxy.concat(config.permissions.commands); // Compile permission flags into a single array
         const inviteUrl = await client.generateInvite(flags); // Generate invite URL with proper permissions
-        return msg.channel.send(utils.successEmbed().addField("Invite link!", `[Click here to invite this bot to your server!](${inviteUrl})`));
+        const serverUrl = "https://discord.gg/N5cBcp3"; // Permanent invite link to official server
+        const embed = utils.successEmbed();
+        embed.setAuthor(guildMember ? `${guildMember.displayName} (${author.tag})` : author.tag, author.avatarURL);
+        embed.addField("Invite me!", `[Click here to invite this bot to your server!](${inviteUrl})`);
+        embed.addField("Get help and support!", `[Click here to join the support server!](${serverUrl})`);
+        return msg.channel.send(embed);
     }
 }
