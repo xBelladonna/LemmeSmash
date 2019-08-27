1e073199bc09459b672450e0855fb0b38e624026
xBelladonna <isabelladonnamoore@outlook.com>
2019-08-27 19:47:06 +0930

    Increase max keysmash length to 32 characters

--- bot/keysmash.js
+++ bot/keysmash.js
@@ -4,7 +4,7 @@ module.exports = {
         // like jhfgsjfhgdgjfhdsgkadfgldskgfhj if given the string "asdfghjkl"
 
         if (!charset) return "ಠ_ಠ";
-        let length = randomValueBetween(16, 24);
+        let length = randomValueBetween(16, 32);
         let keysmash = "";
 
         for (let i = 0; i < length; i++) {
@@ -30,12 +30,12 @@ module.exports = {
         let keysmash = thingToRepeat.repeat(repetitions);
 
         // add extra stuff to the beginning and end about 30% of the time
-        if (Math.random() < 0.3) keysmash = thingToRepeat.substring(1, Math.random(1, 3)) + keysmash;
-        if (Math.random() < 0.3) keysmash += thingToRepeat.substring(0, Math.random(0, 2));
-        // let's just remove random bits, a bit, maybe
-        if (Math.random() < 0.6)
+        if (Math.random() <= 0.3) keysmash = thingToRepeat.substring(1, Math.random(1, 3)) + keysmash;
+        if (Math.random() <= 0.3) keysmash += thingToRepeat.substring(0, Math.random(0, 2));
+        // let's just remove random bits, a bit, maybe, about 60% of the time
+        if (Math.random() <= 0.6)
             for (let i = 0; i < randomValueBetween(0, 4); i++)
-                keysmash = strReplaceAt(keysmash, randomValueBetween(0, keysmash.length), "");
+                keysmash = stringReplaceAt(keysmash, randomValueBetween(0, keysmash.length), "");
 
         return keysmash;
     }
@@ -43,15 +43,11 @@ module.exports = {
 
 
 // Utility functions sdfhadshkghdas
-function randomValueBetween(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const randomValueBetween = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-function randomValueFromArray(array) {
-    return array[Math.floor(Math.random() * array.length)];
-}
+const randomValueFromArray = array => array[Math.floor(Math.random() * array.length)];
 
-function strReplaceAt(string, index, char) {
+function stringReplaceAt(string, index, char) {
     let splitString = string.split("");
     splitString[index] = char;
     return splitString.join("");
